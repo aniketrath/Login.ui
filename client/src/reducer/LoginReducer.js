03f3115ca3196e526fab2176ef51c797b2815eef
aniketrath <aniketrath1121@gmail.com>
2022-09-22 21:15:59 +0530

    setup reducer function and server

---
+++ client/src/reducer/LoginReducer.js
@@ -0,0 +1,54 @@
+const DETAILS = {
+    USERNAME: 'username',
+    PASSWORD: 'password',
+    ERROR: 'error',
+
+}
+
+const DETAILS_DEFAULT = {
+    [DETAILS.USERNAME]: '',
+    [DETAILS.PASSWORD]: '',
+    [DETAILS.ERROR]: '',
+}
+
+const saveDetails = (details) => {
+    localStorage.setItem(DETAILS.USERNAME, details[DETAILS.USERNAME]);
+    localStorage.setItem(DETAILS.PASSWORD, details[DETAILS.PASSWORD]);
+}
+
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case DETAILS.PASSWORD: {
+            if (isNaN(action.payload)) {
+                return state;
+            }
+            return {
+                ...state,
+                [DETAILS.ERROR]: '',
+                [action.type]: action.payload,
+            }
+        }
+
+        case DETAILS.USERNAME:
+            return {
+                ...state,
+                [DETAILS.ERROR]: '',
+                [action.type]: action.payload,
+            }
+        case DETAILS.ERROR:
+            return {
+                ...state,
+                [action.type]: action.payload,
+            }
+
+        default: throw new Error("Unhandled state");
+    }
+}
+
+export {
+    DETAILS,
+    DETAILS_DEFAULT,
+    saveDetails,
+    reducer
+}
