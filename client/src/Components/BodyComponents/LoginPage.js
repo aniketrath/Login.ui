2d7b3a5fe82023b1c5b4c99ec8891625b68a3b60
aniketrath <aniketrath1121@gmail.com>
2022-09-22 21:16:21 +0530

    reducer function and server setup

--- client/src/Components/BodyComponents/LoginPage.js
+++ client/src/Components/BodyComponents/LoginPage.js
@@ -1,121 +1,96 @@
-import React, { useState, useRef, useEffect } from 'react'
-import Dash from './Dashoard';
+import React, { useRef, useEffect, useReducer } from 'react'
 import axios from 'axios';
+import { DETAILS, DETAILS_DEFAULT, reducer, saveDetails } from '../../reducer/LoginReducer';
+import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
+
+    const navigate = useNavigate()
+
+
 
     const userRef = useRef();
     const errRef = useRef();
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [errormsg, setErrorMsg] = useState("");
-    const [success, setSuccess] = useState(false);
+    const [details, dispatch] = useReducer(reducer, DETAILS_DEFAULT);
 
-    const Uname = useRef();
-    const Upwd = useRef();
-    const Uemail = useRef();
-    const Uposition = useRef();
     const api = axios.create({
-        baseURL: `http://localhost:5000/user/`
+        baseURL: `http://localhost:5000/`
     });
 
     useEffect(() => {
-        api.get('/login')
-            .then(res => {
-                Uemail.current = res.data.userEmail;
-                Uname.current = res.data.userName;
-                Upwd.current = res.data.userPassword;
-                Uposition.current = res.data.userPosition;
-            })
-    })
-
-    useEffect(() => {
-        userRef.current.focus();
+        userRef.current?.focus();
     }, [])
 
 
-    useEffect(() => {
-        setErrorMsg('');
-    }, [username, password])
 
     const handleUser = event => {
-        const result = event.target.value;
-        setUsername(result);
+        dispatch({ type: DETAILS.USERNAME, payload: event.target.value })
     }
 
     const handlePassCode = event => {
-        const result = event.target.value.replace(/[^0-9]/gi, "");
-        setPassword(result);
+        dispatch({ type: DETAILS.PASSWORD, payload: event.target.value })
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setUsername('');
-        setPassword('');
-        if (Uemail.current === username && Upwd.current === password) {
-            setSuccess(true);
-        }
+        api.post('/user/login', details)
+            .then((res) => {
+                saveDetails(res.data);
+                navigate("/dashboard")
+            }).catch((err) => {
+                dispatch({ type: DETAILS.ERROR, payload: err.response.data })
+            })
 
     };
-    const userCred = {
-        email: Uemail.current,
-        name: Uname.current,
-        position: Uposition.current
-    };
-
+    console.log(details);
     return (
-        <>{success ? (
-            <Dash
-                Credentials={userCred}
-            />
-        ) : (
-            <section className='w-1/3 py-20 pr-28'>
-                <div className='py-2'>
-                    <h1 className='text-[4rem] font-bold'>
-                        Welcome Back
-                    </h1>
-                    <h2 className='text-lg'>
-                        Please enter your Details.
-                    </h2>
-                    <h2 ref={errRef} aria-live='assertive' id='errormsg' className='text-lg text-red-400'>
-                        {errormsg}
-                    </h2>
+
+        <section className='w-1/3 py-20 pr-28'>
+            <div className='py-2'>
+                <h1 className='text-[4rem] font-bold'>
+                    Welcome Back
+                </h1>
+                <h2 className='text-lg'>
+                    Please enter your Details.
+                </h2>
+                <h2 ref={errRef} aria-live='assertive' id='errormsg' className='text-lg text-red-400'>
+                    {details[DETAILS.ERROR]}
+                </h2>
+            </div>
+            <form method="post">
+                <div className='py-2 pr-4 '>
+                    <label htmlFor='email' className='text-sm'>Email:</label>
+                    <input required onChange={handleUser} value={details[DETAILS.USERNAME]}
+                        className='h-12 min-w-full px-4 rounded-xl
+                            bg-gray-500 placeholder:text-gray-200'
+                        type="email" id="email" placeholder='Enter your Email.' />
                 </div>
-                <form method="post">
-                    <div className='py-2 pr-4 '>
-                        <label htmlFor='email' className='text-sm'>Email:</label>
-                        <input required ref={userRef} onChange={handleUser} value={username}
-                            className='h-12 min-w-full px-4 rounded-xl
+                <div className='py-2 pr-4'>
+                    <label htmlFor='password' className='text-sm'>Password:</label>
+                    <input onChange={handlePassCode} value={details[DETAILS.PASSWORD]}
+                        className='h-12 min-w-full px-4 rounded-xl
                             bg-gray-500 placeholder:text-gray-200'
-                            type="email" id="email" placeholder='Enter your Email.' />
-                    </div>
-                    <div className='py-2 pr-4'>
-                        <label htmlFor='password' className='text-sm'>Password:</label>
-                        <input onChange={handlePassCode} value={password}
-                            className='h-12 min-w-full px-4 rounded-xl
-                            bg-gray-500 placeholder:text-gray-200'
-                            type="password" id="password" placeholder='Password.' />
-                    </div>
-                    <div className='flex min-w-full py-2 items-center pr-4'>
-                        <input className='mx-2' type="checkbox" name="RememberMe" id="RememberMe" />
-                        <label htmlFor="RememberMe">Remember Me &nbsp;</label>
-                        <a className='ml-44' href="/">Forgot Password?</a>
-                    </div>
-                    <div className='pr-4'>
-                        <button onClick={handleSubmit} className='bg-gray-500 text-xl py-2 px-20 
+                        type="password" id="password" placeholder='Password.' />
+                </div>
+                <div className='flex min-w-full py-2 items-center pr-4'>
+                    <input className='mx-2' type="checkbox" name="RememberMe" id="RememberMe" />
+                    <label htmlFor="RememberMe">Remember Me &nbsp;</label>
+                    <a className='ml-44' href="/">Forgot Password?</a>
+                </div>
+                <div className='pr-4'>
+                    <button onClick={handleSubmit} className='bg-gray-500 text-xl py-2 px-20 
                         rounded-xl text-gray-50 min-w-full'
-                            type="submit">Sign In
-                        </button>
-                    </div>
-                    <div className='py-4 flex '>
-                        <h2 className='text-center'>Don't have an Account ? &nbsp;</h2>
-                        <a href="/newuser"> Sign up.</a>
-                    </div>
-                </form>
-            </section>
-        )}
-        </>
+                        type="submit">Sign In
+                    </button>
+                </div>
+                <div className='py-4 flex '>
+                    <h2 className='text-center'>Don't have an Account ? &nbsp;</h2>
+                    <a href="/newuser"> Sign up.</a>
+                </div>
+            </form>
+        </section>
+
     )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 // Initialise Express.
 const express = require('express');
+const cors = require("cors")
 
 const app = express();
 const port = 5000;
 
-app.use((req, res, next) => {
-    res.set('Access-Control-Allow-Origin', '*');
+app.use(cors())
+app.use(express.json())
 
-    next();
-});
 
-const userDetails =
+const USER_DETAILS =
 {
     userEmail: 'benjamin@login.ui.com',
     userPassword: '9556479373',
@@ -18,10 +17,20 @@ const userDetails =
     userPosition: 'Site Developer'
 }
 
-console.log(userDetails);
 
-app.get("/user/login", (req, res) => {
-    res.json(userDetails);
+app.post("/user/login", (req, res) => {
+    if (
+        !req.body.username || !req.body.password
+    ) {
+        return res.status(400).json("Username and Password Required.")
+    }
+
+    if (req.body.username === USER_DETAILS.userName && req.body.password === USER_DETAILS.userPassword) {
+        res.status(200).json(USER_DETAILS);
+    } else {
+        res.status(404).json("Invalid Cred.")
+    }
+
 })
 app.get("/user/logindetails", (req, res) => {
     res.json(req.username, req.password);
